3b260fe9103f595f9a71f40b217e58d1cb79d778
Cotter <cotter.phinney@gmail.com>
2024-07-12 17:02:30 -0400

    init commit (Lecture 1)

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,15 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
         }
+        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
         .padding()
     }
 }
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    
+    var body: some View {
+        ZStack {
+            if (isFaceUp) {
+                RoundedRectangle(cornerRadius: 25)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 25)
+                    .strokeBorder(lineWidth: 3)
+                Text("😱").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 25)
+            }
+        }
+    }
+}
+
+
+
 
 #Preview {
     ContentView()
